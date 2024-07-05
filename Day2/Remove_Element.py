8ca29b0ccab1121e87bdfa6d5d5c197526a1e003
Aamir <yakubaliashwaan@gmail.com>
2024-07-05 22:45:33 +0530

    Update Remove_Element.py

--- Day2/Remove_Element.py
+++ Day2/Remove_Element.py
@@ -1,6 +1,5 @@
 class Solution(object):
-    def removeDuplicates(self, nums):
-        nums[:]={i for i in nums}
+    def removeElement(self, nums, val):
+        nums[:]=[i for i in nums if i!=val]
         return len(nums)
-        
         
